2f0e123e9bf87ff5dbaac21ac0395b1eaa497c86
Abel Surreira <surreira@gmail.com>
2024-06-09 10:45:13 +0100

    chore: update spaceship configuration
    -  Customize spaceship prompt symbols and directory truncation
    -  Add battery status to the prompt
    -  Adjust git branch display settings

--- spaceship/.config/spaceship/spaceship.zsh
+++ spaceship/.config/spaceship/spaceship.zsh
@@ -1,22 +1,31 @@
 #!/bin/zsh
 
-SPACESHIP_TIME_SHOW=true
-SPACESHIP_CHAR_SYMBOL="❯ "
-if [[ $OSTYPE = (linux)* ]]; then
-	SPACESHIP_CHAR_PREFIX="∫ "
-elif [[ $OSTYPE = (darwin)* ]]; then
-	SPACESHIP_CHAR_PREFIX="λ "
-fi
-
 spaceship remove docker
 spaceship remove docker_compose
 spaceship remove react
 spaceship remove package
 spaceship remove node
 spaceship remove async
-spaceship add docker --order rprompt
+spaceship remove battery
+
 spaceship add docker_compose --order rprompt
 spaceship add react --order rprompt
 spaceship add package --order rprompt
 spaceship add node --order rprompt
 spaceship add async --order rprompt
+spaceship add battery --order rprompt
+
+# Sections customization
+SPACESHIP_CHAR_SYMBOL="❯ "
+SPACESHIP_DIR_TRUNC=3
+SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_GIT_BRANCH_ASYNC=true
+SPACESHIP_GIT_STATUS_PREFIX=" "
+SPACESHIP_GIT_STATUS_SUFFIX=""
+SPACESHIP_GIT_SYMBOL=""
+SPACESHIP_GIT_BRANCH_COLOR="gray"
+SPACESHIP_GIT_PREFIX=""
+SPACESHIP_EXEC_TIME_PREFIX=""
+SPACESHIP_NODE_PREFIX=""
+SPACESHIP_REACT_PREFIX=""
+SPACESHIP_DOCKER_COMPOSE_PREFIX=""
