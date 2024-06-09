1fc3437a6a7ddfedae84dc6da07f061cc5f27a59
Abel Surreira <surreira@gmail.com>
2024-06-09 22:09:30 +0100

    chore: update spaceship configuration
    -  Add python and venv to rprompt order
    -  Set SPACESHIP_DIR_TRUNC_REPO to true
    -  Add SPACESHIP_PYTHON_PREFIX and SPACESHIP_VENV_PREFIX

--- spaceship/.config/spaceship/spaceship.zsh
+++ spaceship/.config/spaceship/spaceship.zsh
@@ -5,20 +5,23 @@ spaceship remove docker_compose
 spaceship remove react
 spaceship remove package
 spaceship remove node
+spaceship remove python
+spaceship remove venv
 spaceship remove async
 spaceship remove battery
 
 spaceship add docker_compose --order rprompt
 spaceship add react --order rprompt
-spaceship add package --order rprompt
 spaceship add node --order rprompt
-spaceship add async --order rprompt
+spaceship add python --order rprompt
+spaceship add venv --order rprompt
 spaceship add battery --order rprompt
+spaceship add async --order rprompt
 
 # Sections customization
 SPACESHIP_CHAR_SYMBOL="❯ "
 SPACESHIP_DIR_TRUNC=3
-SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_DIR_TRUNC_REPO=true
 SPACESHIP_GIT_BRANCH_ASYNC=true
 SPACESHIP_GIT_STATUS_PREFIX=" "
 SPACESHIP_GIT_STATUS_SUFFIX=""
@@ -29,3 +32,5 @@ SPACESHIP_EXEC_TIME_PREFIX=""
 SPACESHIP_NODE_PREFIX=""
 SPACESHIP_REACT_PREFIX=""
 SPACESHIP_DOCKER_COMPOSE_PREFIX=""
+SPACESHIP_PYTHON_PREFIX=""
+SPACESHIP_VENV_PREFIX=""
