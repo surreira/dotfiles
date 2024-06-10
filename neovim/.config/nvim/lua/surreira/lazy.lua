edbe256fd1fe4c1b20e245d16cd0778e1a9ef61c
Abel Surreira <surreira@gmail.com>
2024-06-10 17:17:56 +0100

    chore: update LuaSnip configuration
    -  Implement friendly-snippets configuration
    -  Configure lazy loading from VSCode for improved performance
    -  Include cmp-path for path completion in neovim

--- neovim/.config/nvim/lua/surreira/lazy.lua
+++ neovim/.config/nvim/lua/surreira/lazy.lua
@@ -56,10 +56,20 @@ local plugins = {
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
-            'L3MON4D3/LuaSnip',
+            {
+                'L3MON4D3/LuaSnip',
+                dependencies = {
+                    {
+                        'rafamadriz/friendly-snippets',
+                        config = function()
+                            require('luasnip.loaders.from_vscode').lazy_load()
+                        end,
+                    },
+                },
+            },
             'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
-            'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-path',
         },
     },
 
