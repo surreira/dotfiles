a0ba2edbae4766aa0a31138261a7695e7a808576
Abel Surreira <surreira@gmail.com>
2024-10-07 11:55:56 +0100

    feat: add Blade template support for Treesitter
    -  configure Treesitter to recognize Blade filetypes
    -  create highlight and injection queries for Blade syntax
    -  set up parser configuration for Blade using tree-sitter
    -  add filetype detection for Blade files with .blade.php extension

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -53,3 +53,21 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 	},
 })
+
+---@class ParserConfig
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.blade = {
+	install_info = {
+		url = "https://github.com/EmranMR/tree-sitter-blade",
+		files = { "src/parser.c" },
+		branch = "main",
+	},
+	filetype = "blade",
+}
+
+vim.filetype.add({
+	pattern = {
+		[".*%.blade%.php"] = "blade",
+	},
+})
