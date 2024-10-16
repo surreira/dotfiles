092ae684e786193649249848e3e0da6bc496d58f
Abel Surreira <surreira@gmail.com>
2024-10-16 11:53:37 +0100

    feat: add mason.nvim plugin and update lazy setup
    -  integrate mason.nvim configuration
    -  disable notifications for the lazy.nvim checker

--- neovim/.config/nvim/lua/surreira/lazy.lua
+++ neovim/.config/nvim/lua/surreira/lazy.lua
@@ -15,12 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{
-		import = "surreira.plugins",
-	},
+require("lazy").setup({ { import = "surreira.plugins" } }, {
 	checker = {
 		enabled = true,
+		notify = false,
 	},
 	ui = {
 		backdrop = 100,
--- neovim/.config/nvim/lua/surreira/set.lua
+++ neovim/.config/nvim/lua/surreira/set.lua
@@ -1,4 +1,3 @@
--- Set <space> as the leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
