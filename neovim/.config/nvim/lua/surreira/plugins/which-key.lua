9145d2de3aa45363add19a39ae3a227f1a4f6987
Abel Surreira <surreira@gmail.com>
2024-10-18 16:57:15 +0100

    feat(neovim): enhance which-key configuration
    -  add preset option set to "helix" for improved UI
    -  define a new spec for search group under leader key
    -  configure icons to use Nerd Font if available

--- neovim/.config/nvim/lua/surreira/plugins/which-key.lua
+++ neovim/.config/nvim/lua/surreira/plugins/which-key.lua
@@ -2,8 +2,12 @@ return {
 	"folke/which-key.nvim",
 	event = "VimEnter",
 	opts = {
+		preset = "helix",
 		icons = {
 			mappings = vim.g.have_nerd_font,
+		},
+		spec = {
+			{ "<leader>s", group = "[S]earch" },
 		},
 	},
 }
