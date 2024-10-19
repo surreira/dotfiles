2059b090be00c58c693d483f02471ec665a241bd
Abel Surreira <surreira@gmail.com>
2024-10-19 14:39:57 +0100

    refactor(neovim): improve key mappings and descriptions
    -  Organize telescope key mappings under a dedicated section.
    -  Add more key mappings for searching help tags, keymaps, and git files.
    -  Enhance clarity of key mapping descriptions for better usability.
    -  Update diagnostics key mappings for quick access to diagnostic lists.
    -  Refactor file explorer and resizing commands with improved descriptions.

--- neovim/.config/nvim/lua/surreira/remap.lua
+++ neovim/.config/nvim/lua/surreira/remap.lua
@@ -23,26 +23,34 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "[K] Move visual selection
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Buffer scroll [D]own" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Buffer scroll [U]p" })
 
--- Navigation remaps
-vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+-- Telescope Keymaps
+-- [S]earch
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>si", function()
+	builtin.grep_string({ search = vim.fn.input("Search Word > ") })
+end, { desc = "[S]earch [I]nput word" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sv", builtin.git_files, { desc = "[S]earch [V]ersioned git files" })
+vim.keymap.set("n", "<leader>sf", function()
+	builtin.find_files({ hidden = true })
+end, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files" })
 vim.keymap.set("n", "<leader>/", function()
 	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 		winblend = 10,
 		previewer = false,
 	}))
 end, { desc = "[/] Fuzzily search in current buffer" })
-vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sw", function()
-	require("telescope.builtin").grep_string({ search = vim.fn.input("Search Word > ") })
-end, { desc = "[S]earch [W]ord in workspace" })
 
--- Diagnostics keymaps
+-- Diagnostics Keymaps
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics [Q]uickfix list" })
 
 -- Block "." in normal mode to prevent repeating the last change
 vim.api.nvim_set_keymap("n", ".", "<Nop>", { noremap = true })
@@ -59,10 +67,10 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 -- File tree keymaps
 vim.keymap.set("n", "<leader>fe", function()
 	require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-end, { desc = "Open the [F]ile [E]xplorer" })
+end, { desc = "[F]ile [E]xplorer" })
 
 -- Resize split buffers
-vim.keymap.set("n", "<leader>+", "<cmd>resize +5<CR>")
-vim.keymap.set("n", "<leader>-", "<cmd>resize -5<CR>")
-vim.keymap.set("n", "<leader>º", "<cmd>vert res +5<CR>")
-vim.keymap.set("n", "<leader>ª", "<cmd>vert res -5<CR>")
+vim.keymap.set("n", "<leader>+", "<cmd>resize +5<CR>", { desc = "Increase vertical buffer size" })
+vim.keymap.set("n", "<leader>-", "<cmd>resize -5<CR>", { desc = "Decrease vertical buffer size" })
+vim.keymap.set("n", "<leader>º", "<cmd>vert res +5<CR>", { desc = "Increase horizontal buffer size" })
+vim.keymap.set("n", "<leader>ª", "<cmd>vert res -5<CR>", { desc = "Decrease horizontal buffer size" })
