de1b0db40e894e2770deea5ada45ba6807256773
Abel Surreira <surreira@gmail.com>
2024-10-19 14:37:10 +0100

    feat(neovim): enhance gitsigns keymaps and actions
    -  add signs for staged changes
    -  refactor key mappings for better readability
    -  include additional commands for staging and resetting hunks

--- neovim/.config/nvim/lua/surreira/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/surreira/plugins/gitsigns.lua
@@ -9,21 +9,34 @@ return {
 			topdelete = { text = "‾" },
 			changedelete = { text = "~" },
 		},
+		signs_staged = {
+			add = { text = "+" },
+		},
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 
-			local function nmap(mode, l, r, opts)
-				opts = opts or {}
+			local function nmap(mode, l, r, desc)
+				local opts = {}
 				opts.buffer = bufnr
+				opts.desc = desc
 				vim.keymap.set(mode, l, r, opts)
 			end
 
 			-- Navigation
-			nmap("n", "<leader>gn", gs.next_hunk, { desc = "[G]o to [N]ext Hunk" })
-			nmap("n", "<leader>gp", gs.prev_hunk, { desc = "[G]o to [P]revious Hunk" })
+			nmap("n", "<leader>gn", gs.next_hunk, "[G]it go to [N]ext Hunk")
+			nmap("n", "<leader>gp", gs.prev_hunk, "[G]it go to [P]revious Hunk")
 
 			-- Actions
-			nmap("n", "<leader>ph", gs.preview_hunk, { desc = "[P]review [H]unk" })
+			nmap("n", "<leader>gh", gs.preview_hunk, "[P]review [H]unk")
+			nmap("v", "<leader>gs", function()
+				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end, "[G]it [S]tage Hunk")
+			nmap("v", "<leader>gr", function()
+				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end, "[G]it [R]eset Hunk")
+			nmap("n", "<leader>gu", gs.undo_stage_hunk, "[G]it [U]ndo staged Hunk")
+			nmap("n", "<leader>gS", gs.stage_buffer, "[G]it [S]tage Buffer")
+			nmap("n", "<leader>gR", gs.reset_buffer, "[G]it [R]eset Buffer")
 		end,
 	},
 }
